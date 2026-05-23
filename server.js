5d710963e6bfaff1241261b10c8fa4b6a160563a
kvr-ui <kvr@focasedu.com>
2026-05-23 17:40:30 +0530

    fix

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(cors({
     "http://localhost:3000",
     "http://localhost:5173",
     "http://localhost:5174",
+    "http://localhost:5175",
     "https://admin-focas.netlify.app",
     "https://combo-focas.netlify.app",
     "https://focas.vercel.app",
