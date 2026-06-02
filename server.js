3601aa22407dae30006991d73e505d00d1b647de
kvr-ui <kvr@focasedu.com>
2026-06-02 12:17:46 +0530

    fix

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ app.use(cors({
     "https://lms-student-focas.vercel.app",
     "https://lms-admin-focastech.vercel.app",
     "https://lms-accounts-admin.vercel.app",
+    "https://lms-accounts-admin-git-main-focas.vercel.app",
+    "https://lms-accounts-admin-n6599kmnq-focas.vercel.app",
     "https://app.focasedu.com",,
     "https://admin.focasedu.com",
     "https://lms.focasedu.com",
